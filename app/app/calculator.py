b17f3457418cdd8552da98b9cfd68759329a1fae
Garib <garild@o2.pl>
2025-01-05 14:31:49 +0100

    Lesson-29: updated gitignore, added Test

---
+++ app/app/calculator.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+def add(a, b):
+    "Add two numbers"
+    return a + b
+def sub(a, b):
+    "Subtract two numbers"
+    return a - b
+def multiply(a,b):
+     "Multiply two numbers"
+     return a*b
