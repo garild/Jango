6627d3b9354d4bca23a9f1e095f2179e426df2f3
Garib <garild@o2.pl>
2025-01-06 19:17:34 +0100

    Lesson 38: Django db configuration

--- app/tests/test_module.py
+++ app/tests/test_module.py
@@ -11,9 +11,11 @@ class TestCalculator(SimpleTestCase):
     def test_add_two_numbers(self):
         result = calculator.add(2,3)
         self.assertEqual(result, 5)
+
     def test_subtract_two_numbers(self):
         result = calculator.sub(5,2)
         self.assertEqual(result, 3)
+
     def test_multiple_numbers(self):
         "Test case will multiply 2 numbers"
         result = calculator.multiply(2,3)
