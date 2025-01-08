8799b93bac2cc7b18dcb0097f46dacda68452b15
Garib <garild@o2.pl>
2025-01-08 16:39:01 +0100

    Fixed lint issue

--- app/app/calculator.py
+++ app/app/calculator.py
@@ -2,12 +2,17 @@
 Calculator functions
 """
 
+
 def add(a, b):
     "Add two numbers"
     return a + b
+
+
 def sub(a, b):
     "Subtract two numbers"
     return a - b
-def multiply(a,b):
-     "Multiply two numbers"
-     return a*b
+
+
+def multiply(a, b):
+    "Multiply two numbers"
+    return a * b
