3b51bf950ecb59b456877aee64bd7727358c959d
Garib <garild@o2.pl>
2025-01-11 21:44:04 +0100

    Lesson 48: implement user model. Test emial normalized

---
+++ app/tests/test_models.py
@@ -0,0 +1,34 @@
+"""
+Test for models
+"""
+
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class TestUserModel(TestCase):
+    """Test models."""
+
+    def test_create_user_with_email_successfull(self):
+        """Test create a user with an email is successfull."""
+        email = 'tests@example.pl'
+        password = 'test123'
+        user = get_user_model().objects.create_user(
+            email=email, password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test if new user email is normalized"""
+
+        sameple_emails = [
+            ['test@EXSAMPLE.com', 'test@exsample.com'],
+            ['TEST2@EXAmple.pl', 'TEST2@example.pl'],
+            ['TEST1@GMAIL.COM', 'TEST1@gmail.com']
+        ]
+        for email, expected in sameple_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
